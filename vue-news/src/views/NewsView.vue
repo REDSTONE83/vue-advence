fff70d06deaa4270a151d0c551cdc611f5714f89
Hongseok Kang <redstone8311@gmail.com>
2020-12-18 01:11:47 +0900

    Update

---
+++ vue-news/src/views/NewsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="user in users" :key="user.id">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(response => this.users = response.data)
+      .catch(error => console.log(error));
+  }
+}
+</script>
+
+<style>
+
+</style>
